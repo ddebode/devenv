2c03a8b6be03dd7fc5774f4ea171a592e7745ad8
ddebode <ddebode@gmail.com>
2019-07-28 13:37:22 +0200

    added oh-my-zsh

--- install-apt.sh
+++ install-apt.sh
@@ -7,3 +7,6 @@ sudo apt-get install lazygit
 sudo apt install rofi
 sudo add-apt-repository ppa:mmstick76/alacritty
 sudo apt install alacritty  
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
