3142cf6d5a7d2bb8f46ba951554c61ddf4ad5ecc
ddebode <ddebode@gmail.com>
2019-07-28 13:21:36 +0200

    added alacritty

--- install-apt.sh
+++ install-apt.sh
@@ -4,3 +4,6 @@ sudo apt install vim-gtk
 sudo add-apt-repository ppa:lazygit-team/release
 sudo apt-get update
 sudo apt-get install lazygit
+sudo apt install rofi
+sudo add-apt-repository ppa:mmstick76/alacritty
+sudo apt install alacritty  
