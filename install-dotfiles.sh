6a746aaa6a9c0e2e60f62eef5373078dd4b14d30
ddebode <ddebode@bol.com>
2019-09-09 11:10:09 +0200

    added kube-tmux

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -5,3 +5,4 @@ bashdot install dotfiles
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/jonmosco/kube-tmux ~/.tmux/kube-tmux
