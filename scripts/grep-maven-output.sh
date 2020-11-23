e268d3222a0d26de8068c98a387ff1fac1634b11
ddebode <ddebode@bol.com>
2020-11-23 14:29:46 +0100

    Fixed typo and added [ERROR] failures to maven grep script

--- scripts/grep-maven-output.sh
+++ scripts/grep-maven-output.sh
@@ -10,4 +10,4 @@ echo "";
 echo -e "\e[32m--- FINISHED---  \033[0m";
 echo "";
 
-echo "$output" | rg -A 10 "(FAILURE!|COMPILATION ERROR|BUILD SUCCES)";
+echo "$output" | rg -A 10 "(FAILURE!|COMPILATION ERROR|\[ERROR\] Failures:|BUILD SUCCESS)";
