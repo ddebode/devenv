5433aa9181c8237ef5ab2e6b4dce0f3cb499e4c6
ddebode <ddebode@bol.com>
2020-11-27 09:20:47 +0100

    Added some explanation to maven grep script

--- scripts/grep-maven-output.sh
+++ scripts/grep-maven-output.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Script which you can pipe the maven output into and it will grep the interesting stuff for you at the end!
+# This script is using rip-grep
+# To keep the colors in the maven output use unbuffer (package expect-dev): unbuffer mvn clean install | <this script>
 
 output="";
 while read line; do
