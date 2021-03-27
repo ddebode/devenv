1b65d48e70bd1247649cdd15019c3b4dfeec3ec6
ddebode <ddebode@bol.com>
2021-03-27 16:19:32 +0100

    Add install script for macbook

---
+++ install-brew-packages.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+brew install --cask spectacle
+brew install --cask alfred
+# brew install --cask iterm2
+brew install vifm
+brew install tmux # tmux attach -t base || tmux new -s base
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+brew install node
+brew install antigen
+brew install neovim
+brew install tldr
+brew install lazygit
+brew install the_silver_searcher
+brew install lsd
+brew install fd
+brew install rm-improved
+
+# Java
+brew tap adoptopenjdk/openjdk
+brew install --cask adoptopenjdk8
+brew install --cask adoptopenjdk12
+brew install maven
+# Add to zshrc
+# alias j12="export JAVA_HOME=`/usr/libexec/java_home -v 12`"
+# alias j8="export JAVA_HOME=`/usr/libexec/java_home -v 1.8`"
+brew install --cask jetbrains-toolbox
+
