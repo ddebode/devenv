6435d86fbd42bf3c6523523bb270bff0b34a0911
ddebode <ddebode@bol.com>
2021-10-21 09:29:45 +0200

    Created script to switch java version depending on version in pom

---
+++ scripts/find-java-version-in-pom.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This scripts checks the java version in the pom file in the same folder and then switches to correct java version
+# scripts needs ripgrep
+# Put this in your zshrc or bashrc
+# # Ls after every cd and check java version
+# function cd {
+#    builtin cd "$@" && ls -F && source ~/path_to_script/find-java-version-in-pom.sh
+# }
+#source ~/path_to_srcipt/find-java-version-in-pom.sh
+
+version=$(rg -o '<java.version>.*</java.version' pom.xml | rg -o '[\d]+\.?\d?') 2> /dev/null
+
+if [ "$version" = "12" ]; then
+    echo "Switch to java 12"
+    export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-12.jdk/Contents/Home
+fi
+
+if [ "$version" = "11" ]; then
+    echo "Switch to java 11"
+    export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home
+fi
+
+if [ "$version" = "1.8" ]; then
+    echo "Switch to java 8"
+    export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
+fi
+
